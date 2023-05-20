c8d4faea1e991b175c16102c6de2da7678ea6b8d
Jo Carr <hulaus21@gmail.com>
2023-05-20 01:40:16 -0700

    tried fixing bugs in links to make links more secure but now won't open in new tab

--- jo-carr-portfolio/src/components/SocialMedia.jsx
+++ jo-carr-portfolio/src/components/SocialMedia.jsx
@@ -6,10 +6,10 @@ function SocialLinks() {
     return(
         <>
         <div className="social-container">
-            <a href="https://github.com/hulaus" target="_blank" className="github social">
+            <a href="https://github.com/hulaus"   rel="noreferrer" target="_blank"  className="github social">
                 <FontAwesomeIcon icon={faGithub} size="2x"/>
             </a>
-            <a href="https://www.linkedin.com/in/joseph-carrasco/" target="_blank" className="linkedin social">
+            <a href="https://www.linkedin.com/in/joseph-carrasco/" rel="noreferrer" target="_blank" className="linkedin social">
                 <FontAwesomeIcon icon={faLinkedin} size="2x"/>
             </a>
         </div>
