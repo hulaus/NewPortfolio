c165cfdca7040ffb9f7c1850db425a345db5c954
Jo Carr <hulaus21@gmail.com>
2023-05-20 01:22:57 -0700

    added section under picture and main top section. Added Section with my name and a brief description of myself. I also added a waving hand emoji with styling so it waves

--- jo-carr-portfolio/src/components/TopSection.jsx
+++ jo-carr-portfolio/src/components/TopSection.jsx
@@ -8,12 +8,23 @@ function TopSection() {
         <div className="AboutMe">
         <h1>Full-Stack Developer, Designer & Frontend Enthusiast </h1>
         <img className="MeAtDisney" src={Me} alt="Me at DisneyLand" />
-        <p>As a lifelong learner and passionate coder, I am driven by the desire to grow. With one year of experience, I am dedicated to refining my craft.
+        <p>I am a lifelong learner passsionate about what I do, I am driven by the desire to grow. With one year of experience, I am dedicated to refining my craft.
             I thrive on challenges, consistently seeking opportunities to elevate my skills and reach new levels of exellence !
          </p>
         <div className="SocialLinks">
             <SocialLinks />
         </div>
+        </div>
+        <div>
+            <div className="WhoAmI">
+                <h2>Hello I'm Joseph! <span className="wave">👋</span>  
+                </h2>
+                <p>
+                    For the past year, I have been engaged in the world of full-stack development eager to gain valuable experience. While I may be at the early stages of my journey, my ability to adapt swiftly and my drive make up for any lack of experience.
+                    I approach every coding challenge with curiosity and enthusiasm, constantly pushing myself to learn and grow.
+                    Now, I am excitedly seeking my first real work experience, eager to collaborate with talented individuals and contribute to the creation of exceptional digital products!
+                </p>
+            </div>
         </div>
         </>
     )
